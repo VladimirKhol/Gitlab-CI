3f3753542f0856d1bf50c3fd2876ac659bc9726f
volodymyrkholostyi <volodymyr.kholostyi@logianalytics.com>
2023-06-26 11:48:59 +0300

    Increase page load time

--- src/main/java/org/example/utils/DriverUtil.java
+++ src/main/java/org/example/utils/DriverUtil.java
@@ -4,7 +4,7 @@ import com.codeborne.selenide.Configuration;
 
 public class DriverUtil {
     public static void setCapabilities() {
-        Configuration.pageLoadTimeout = 60000;
+        Configuration.pageLoadTimeout = 120000;
         Configuration.browserSize = "1440x900";
         Configuration.pageLoadStrategy = "eager";
         Configuration.headless = true;
